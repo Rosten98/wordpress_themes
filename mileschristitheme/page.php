16f176dfa242afdfb39c7d1b9067b52843fa6389
Gerardo Rodriguez Delgado <gerardorodriguezdelgado@MacBook-Pro-de-Gerardo.local>
2021-07-03 14:20:12 -0500

    Customize single and post php files

--- mileschristitheme/page.php
+++ mileschristitheme/page.php
@@ -3,11 +3,16 @@
 <?php if(have_posts()){
   while(have_posts()){
     the_post();?>
-<header class="post-thumbnail mt-100">
-
+    <?php 
+      if(has_post_thumbnail()){
+     ?>
+<header class="post-thumbnail mt-100 mb-100n">
   <?php the_post_thumbnail('large')?>
-  </header>
-<main class="mc-container">
+</header>
+     <?php   
+      }
+    ?>
+<main class="mc-container mt-200">
       <h1 class="my-20"><?php the_title()?></h1>
       <div class="my-20 post-content">
         <?php the_content()?>
--- mileschristitheme/single.php
+++ mileschristitheme/single.php
@@ -1,1 +1,25 @@
-<h1>Articles</h1>
+<?php get_header(); ?>
+
+<?php if(have_posts()){
+  while(have_posts()){
+    the_post();?>
+    <?php 
+      if(has_post_thumbnail()){
+     ?>
+<header class="post-thumbnail mt-100 mb-100n">
+  <?php the_post_thumbnail('large')?>
+</header>
+     <?php   
+      }
+    ?>
+<main class="mc-container mt-200">
+      <h1 class="my-20"><?php the_title()?></h1>
+      <div class="my-20 post-content">
+        <?php the_content()?>
+      </div>
+  <?php
+    }
+  }?>
+</main>
+
+<?php get_footer(); ?>
