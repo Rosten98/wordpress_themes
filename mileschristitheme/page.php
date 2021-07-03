fa233c7941571deb6252750a6b72994144e1d111
Gerardo Rodriguez Delgado <gerardorodriguezdelgado@MacBook-Pro-de-Gerardo.local>
2021-07-02 19:53:49 -0500

    Customize blog.php page

--- mileschristitheme/page.php
+++ mileschristitheme/page.php
@@ -1,1 +1,20 @@
-<h1>Pages</h1>
+<?php get_header(); ?>
+
+<?php if(have_posts()){
+  while(have_posts()){
+    the_post();?>
+<header class="post-thumbnail mt-100">
+
+  <?php the_post_thumbnail('large')?>
+  </header>
+<main class="mc-container">
+      <h1 class="my-20"><?php the_title()?></h1>
+      <div class="my-20 post-content">
+        <?php the_content()?>
+      </div>
+  <?php
+    }
+  }?>
+</main>
+
+<?php get_footer(); ?>
