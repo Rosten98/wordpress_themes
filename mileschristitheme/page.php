314d5ac389a9d7e2ce7d905fa6c0d8692336826c
Gerardo Rodriguez <gerardo.rodriguezdelgado@nxp.com>
2021-07-07 18:55:01 -0500

    Add header with thumbnail and title

--- mileschristitheme/page.php
+++ mileschristitheme/page.php
@@ -8,13 +8,18 @@
      ?>
 <header class="post-thumbnail mt-100 mb-100n">
   <?php the_post_thumbnail('large')?>
+  <h1 class=""><?php the_title()?></h1>
 </header>
      <?php   
       }
     ?>
 <main class="mc-container mt-200">
-      <h1 class="my-20"><?php the_title()?></h1>
       <div class="my-20 post-content">
+        <?php if(!has_post_thumbnail()){?>
+          <h1 class=""><?php the_title()?></h1>
+        <?php
+        }
+        ?>
         <?php the_content()?>
       </div>
   <?php
